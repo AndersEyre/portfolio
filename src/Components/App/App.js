32ccc17de2f39e6eb8529307d7307631890cd1ec
anders_eyre <anders_air@hotmail.com>
2020-08-28 11:29:30 +1000

    presentation project component and proxy state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,37 +1,56 @@
 import React from 'react';
 
 import { Nav } from '../Nav/nav';
+import { Project } from '../Project/project';
 import './App.css';
 import '../Nav/nav.css';
 
-function App() {
-  return (
-    
-    <div>
-      <Nav/>
-      <div className='app'>
-        
-        <div className='background-img' id='top-background-img'>  
-          <h1>AE</h1>
-          <p>Development Done Differently.</p>
-          <img src={require('./Portfolio/dani thump.jpg')} alt='background'/>
-        </div>
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      projects:[
+        {
+          id:1,
+          img: '//img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/video/wibbitz/wbz-what-to-do-about-hairballs-in-cats.jpg',
+          codepen: 'codepen.io',
+          github: 'github.com',
+        }
+      ]
+    };
+  
+  }
 
-        <div className='background-img' id='about'>
-          <img src={require('./Portfolio/Cover photo.jpg')} alt='background'/>
-        </div>
+  
+  render(){
+    return (
+      <div>
+        <Nav/>
+        <div className='app'>
+          
+          <div className='background-img' id='top-background-img'>  
+            <h1>AE</h1>
+            <p>Development Done Differently.</p>
+            <img src={require('./Portfolio/dani thump.jpg')} alt='background'/>
+          </div>
 
-        <div className='background-img' id='projects'>
-          <img src={require('./Portfolio/Spain cover.jpg')} alt='background'/>
-        </div>
+          <div className='background-img' id='about'>
+            <Project/>
+            <img src={require('./Portfolio/Cover photo.jpg')} alt='background'/>
+          </div>
 
-        <div className='background-img' id='contact'>
-          <img src={require('./Portfolio/Once more into the breach.jpg')} alt='background' />
-        </div>
+          <div className='background-img' id='projects'>
+            <img src={require('./Portfolio/Spain cover.jpg')} alt='background'/>
+          </div>
 
+          <div className='background-img' id='contact'>
+            <img src={require('./Portfolio/Once more into the breach.jpg')} alt='background' />
+          </div>
+
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 var prevScrollpos = window.pageYOffset;
---
+++ src/Components/Project/project.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function Project() {
+    
+    return (
+        <div>
+            <div>
+                <img alt='Project-img'></img>
+            </div>
+            <h1>Project</h1>
+            <h3><a href='https://codepen.io/'>CodePen</a></h3>
+            <h3><a href='https://github.com/'>GitHub</a></h3>
+        </div>
+
+    );
+};
+
+export default Project;
