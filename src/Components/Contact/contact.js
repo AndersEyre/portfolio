8a5557108a1b3520e9120f560028e42505b5970e
anders_eyre <anders_air@hotmail.com>
2020-09-01 21:14:45 +1000

    img compressed

---
+++ src/Components/Contact/contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './contact.css';
+import { ContactForm } from '../ContactForm/contactForm';
+
+export class Contact extends React.Component {
+
+    render(){
+        return(
+            <section className='contact'>
+                <div>
+
+                </div>
+                <ContactForm/>
+            </section>
+        )
+    }
+}
