00ccade8740bad5881029b3c03943197bbe58624
anders_eyre <anders_air@hotmail.com>
2020-09-13 21:52:18 +1000

    github icon added

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -4,6 +4,11 @@ import '../Styles/Footer/footer.css';
 import fbIcon from '../Styles/Footer/Icons/fb.png';
 import instaIcon from '../Styles/Footer/Icons/insta.png';
 import mailIcon from '../Styles/Footer/Icons/mail.png';
+import gitHub from '../Styles/Footer/Icons/github.png'
+
+import fbIcon2 from '../Styles/Footer/Icons/Facebook Teal.png';
+import instaIcon2 from '../Styles/Footer/Icons/Instagram Teal.png';
+import mailIcon2 from '../Styles/Footer/Icons/Email Teal.png';
 
 
 
@@ -14,6 +19,7 @@ export function Footer() {
         <div className='social-group'>
           <a href='https://www.facebook.com/anders.eyre/' target='_blank' rel='noopener noreferrer'><img src={fbIcon} alt='fb icon' className='icon'></img></a>
           <a href='https://www.instagram.com/anders_air/?hl=en' target='_blank' rel='noopener noreferrer'><img src={instaIcon} alt='insta icon' className='icon'></img></a>
+          <a href='https://github.com/AndersEyre/' target='_blank' rel='noopener noreferrer'><img src={gitHub} alt='insta icon' className='icon' id='github-icon'></img></a>
           <a href='mailto:anders_air@hotmail.com'><img src={mailIcon} alt='mail icon' className='icon'></img></a>
         </div>
         <div>
