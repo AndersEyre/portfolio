c14867944a9002f909e9b8c85a497d75944ce4b0
anders_eyre <anders_air@hotmail.com>
2020-09-16 20:04:12 +1000

    stateless form changed to stateful form

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -2,37 +2,66 @@ import React from 'react';
 import '../Styles/ContactForm/contactForm.css';
 import CloseBtn from '../Styles/ContactForm/Images/close-button.png';
 
+
+
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
 export class ContactForm extends React.Component {
 
-
+    constructor(props) {
+        super(props);
+        this.state = { name: "", email: "", subject: "", message: "" };
+      }
+  
+      /* Here’s the juicy bit for posting the form submission */
+  
+      handleSubmit = e => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", ...this.state })
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+  
+        e.preventDefault();
+      };
+  
+      handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  
+    
     render() {
+        const { name, email, subject, message } = this.state;
 
         return (
             <form 
             id='contact'
-            name='contact'
             method='POST'
-            netlify
+            
             style={this.props.visible ? { visibility: 'visible' } : { visibility: 'hidden' }} >
-                <input type='hidden' name='form-name' value='contact' />
+                
                 <div className='psuedo-background'/>
                 <h1 >GOT AN IDEA?</h1>
                 <img className='close-button' src={CloseBtn} alt='close button' onClick={this.props.handleClick} />
                 <div className='form-group'>
                     <label htmlFor='name'></label><br></br>
-                    <input id='name' name='name' type='text' className='form-control' placeholder='Your Name' required />
+                    <input id='name' name='name' type='text' className='form-control' placeholder='Your Name' value={name} onChange={this.handleChange} required />
                 </div>
                 <div className='form-group'>
                     <label htmlFor='email'></label><br></br>
-                    <input type='email' name='email' id='email' className='form-control' placeholder='Your Email' required />
+                    <input type='email' name='email' id='email' className='form-control'  placeholder='Your Email' value={email} onChange={this.handleChange} required />
                 </div>
                 <div className='form-group'>
                     <label htmlFor='subject'></label><br></br>
-                    <input type='text' name='subject' className='form-control' id='subject' placeholder='Subject' required />
+                    <input type='text' name='subject' className='form-control' id='subject' placeholder='Subject' value={subject} onChange={this.handleChange} required />
                 </div>
                 <div className='form-group'>
                     <label htmlFor='message'></label><br></br>
-                    <textarea className='form-control' id='message' name='message' rows='5' placeholder='Your Message Here...' required></textarea>
+                    <textarea className='form-control' id='message' name='message' rows='5' placeholder='Your Message Here...' value={message} onChange={this.handleChange} required></textarea>
                 </div>
                 <p>All Fields Are Required</p>
                 <button type='submit'>Send It!</button>
