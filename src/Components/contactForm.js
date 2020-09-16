cea89631a7ae442b4fd2cbc73645f3104ecc4288
anders_eyre <anders_air@hotmail.com>
2020-09-16 19:35:44 +1000

    netlify added to jsx form

--- src/Components/contactForm.js
+++ src/Components/contactForm.js
@@ -12,8 +12,9 @@ export class ContactForm extends React.Component {
             id='contact'
             name='contact'
             method='POST'
+            netlify
             style={this.props.visible ? { visibility: 'visible' } : { visibility: 'hidden' }} >
-                <input type='hidden' name='contact' value='contact' />
+                <input type='hidden' name='form-name' value='contact' />
                 <div className='psuedo-background'/>
                 <h1 >GOT AN IDEA?</h1>
                 <img className='close-button' src={CloseBtn} alt='close button' onClick={this.props.handleClick} />
