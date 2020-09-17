73c583350bab8f2a013afeae2ce5ff0c76905cd5
anders_eyre <anders_air@hotmail.com>
2020-09-17 10:59:51 +1000

    npm run build

---
+++ build/precache-manifest.9d5bfd97118086e36e75b4c619feb801.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af59d90046baecca61276cc93e27fa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9948400a5585c2febfbb",
+    "url": "/static/css/main.45fd931f.chunk.css"
+  },
+  {
+    "revision": "953cb7646fa23b4a18ad",
+    "url": "/static/js/2.5ab46e9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5ab46e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9948400a5585c2febfbb",
+    "url": "/static/js/main.b6ec9eda.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1ccb4b5cd712c8d9a168e5223f1853d8",
+    "url": "/static/media/Jammming.1ccb4b5c.png"
+  },
+  {
+    "revision": "ea2e37a78383eb840b88f163e85531a3",
+    "url": "/static/media/Ravenous.ea2e37a7.png"
+  },
+  {
+    "revision": "9276634c06792b3158f8a573cd0ecdac",
+    "url": "/static/media/Resume.9276634c.pdf"
+  },
+  {
+    "revision": "45ff7b259978d3c21d1a30ddb0a8324e",
+    "url": "/static/media/Stockholm-small.45ff7b25.jpg"
+  },
+  {
+    "revision": "4649f629f9a9de1f498578c99866edaf",
+    "url": "/static/media/comfortaa-bold-webfont.4649f629.woff"
+  },
+  {
+    "revision": "a8c1ec910ee3356aaf82439ffc268264",
+    "url": "/static/media/comfortaa-bold-webfont.a8c1ec91.woff2"
+  },
+  {
+    "revision": "ca4327257c0de929429888d4f88a89d3",
+    "url": "/static/media/comfortaa-light-webfont.ca432725.woff"
+  },
+  {
+    "revision": "fd62014f837fb2243c500fe5c309c7bd",
+    "url": "/static/media/comfortaa-light-webfont.fd62014f.woff2"
+  },
+  {
+    "revision": "5f30c1872bbc629ca89db97ac67ee9cc",
+    "url": "/static/media/comfortaa-regular-webfont.5f30c187.woff"
+  },
+  {
+    "revision": "783c57c3b68d834634641f2a79e8d7f7",
+    "url": "/static/media/comfortaa-regular-webfont.783c57c3.woff2"
+  },
+  {
+    "revision": "0baa2ab2867bb082b174d019ec551355",
+    "url": "/static/media/content-creation.0baa2ab2.png"
+  },
+  {
+    "revision": "8adf02d50c6f8082141479919a8f4d50",
+    "url": "/static/media/expresso-screenshot.8adf02d5.png"
+  },
+  {
+    "revision": "3feee8daa83345b8a42e537e1046253a",
+    "url": "/static/media/fb.3feee8da.png"
+  },
+  {
+    "revision": "97bcac2bdb50c676d9569944c055fd92",
+    "url": "/static/media/insta.97bcac2b.png"
+  },
+  {
+    "revision": "678b4e5369855a792f9f87ba836e924c",
+    "url": "/static/media/mail.678b4e53.png"
+  },
+  {
+    "revision": "86e619a62ca3889fe20e2a76d712e3ec",
+    "url": "/static/media/soft-skills.86e619a6.png"
+  }
+]);
